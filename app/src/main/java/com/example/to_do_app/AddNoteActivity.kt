0b28a794e6a671dfbad618ab00752304cd6dbbb2
kukka14 <thakshilagunawardena16@gmail.com>
2024-05-12 18:18:36 +0530

    create a create functionality to add note

--- app/src/main/java/com/example/to_do_app/AddNoteActivity.kt
+++ app/src/main/java/com/example/to_do_app/AddNoteActivity.kt
@@ -1,11 +1,40 @@
 package com.example.to_do_app
 
-import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.Toast
+import androidx.activity.enableEdgeToEdge
+import androidx.appcompat.app.AppCompatActivity
+import androidx.core.view.ViewCompat
+import androidx.core.view.WindowInsetsCompat
+import com.example.to_do_app.databinding.ActivityAddNoteBinding
+
 
 class AddNoteActivity : AppCompatActivity() {
+
+    private lateinit var binding: ActivityAddNoteBinding
+    private lateinit var db: NoteDataBaseHelper
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
+        enableEdgeToEdge()
         setContentView(R.layout.activity_add_note)
-    }
+        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main)) { v, insets ->
+            val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
+            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
+            insets
+        }
+        binding = ActivityAddNoteBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+
+        db = NoteDataBaseHelper(this)
+
+        binding.saveButton.setOnClickListener{
+            val title = binding.titleEditText.text.toString()
+            val content = binding.contentEditText.text.toString()
+            val note = Note(0, title, content)
+            db.insertNote(note)
+            finish()
+            Toast.makeText(this, "Note Saved", Toast.LENGTH_SHORT).show()
+            }
+        }
 }
+
