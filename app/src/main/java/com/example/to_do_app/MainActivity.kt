aae63eb75ea6320e51aa93a01afd7740caba6641
kukka14 <thakshilagunawardena16@gmail.com>
2024-05-12 16:35:38 +0530

    create AddNoteActivity and intent

--- app/src/main/java/com/example/to_do_app/MainActivity.kt
+++ app/src/main/java/com/example/to_do_app/MainActivity.kt
@@ -1,11 +1,24 @@
 package com.example.to_do_app
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import com.example.to_do_app.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
+
+     private lateinit var binding: ActivityMainBinding
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+
+
+        binding = ActivityMainBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+
+        binding.addButton.setOnClickListener{
+            val intent = Intent(this, AddNoteActivity::class.java)
+            startActivity(intent)
+        }
     }
 }
